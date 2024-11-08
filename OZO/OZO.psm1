2b13fd5976af569ab86b6c7fee24074c44bce329
alievertz <andy.lievertz@gmail.com>
2024-11-08 16:32:29 -0700

    Development (#2)
    * readme
    
    * Version 1.0.0 release candidate
    
    ---------
    
    Co-authored-by: Andy Lievertz <alievertz@onezeroone.dev>

--- OZO/OZO.psm1
+++ OZO/OZO.psm1
@@ -3,19 +3,28 @@ Function Get-OZOHostname {
         .SYNOPSIS
         See description.
         .DESCRIPTION
-        Returns the hostname for a given fully qualified domain name ("FQDN")
+        Returns the hostname for a given fully qualified domain name ("FQDN"). If executed without parameters, it returns the hostname of the running system.
         .PARAMETER FQDN
-        The fully qualified domain name
+        The fully qualified domain name.
         .EXAMPLE
         Get-OZOHostname -FQDN "example.contoso.com"
         example
+        .EXAMPLE
+        Get-OZOHostname
+        DESKTOP-OZO80202
         .LINK
-        
+        https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozohostname
     #>
+    # Parameters
+    param ([Parameter(Mandatory=$false,HelpMessage="The fully qualified domain name")][String]$FQDN)
+    # Determine if FQDN is null or empty
+    If ([String]::IsNullOrEmpty($FQDN)) {
+        # FQDN is null or empty; return the local hostname
+        return $Env:COMPUTERNAME
+    } Else {
+        # FQDN is not null or empty; parse and return the hostname
+        return ($FQDN -Split "\.",2)[0]
+    }
 }
 
-Function New-OZO8601Date {
-
-}
-
-Export-ModuleMember -Function Get-OZOHostname,New-OZO8601Date
+Export-ModuleMember -Function Get-OZOHostname
--- README.md
+++ README.md
@@ -1,3 +1,46 @@
-# OZO-PowerShell-Module
+# OZO PowerShell Module Installation and Usage
 
-## Get-OZOHostname
+## Installation
+This module is published to the PowerShell Gallery. Register the repository and then execute the following in an _Administrator_ PowerShell:
+
+`Install-Module OZO`
+
+## Usage
+Import this module in your script or console to make the functions available for use:
+
+`Import-Module OZO`
+
+## Functions
+
+### Get-OZOHostname
+
+#### Description
+Returns the hostname for a given fully qualified domain name ("FQDN"). If executed without parameters, it returns the hostname of the running system.
+
+#### Syntax
+```
+Get-OZOHostname
+    [-FQDN <String>]
+```
+
+#### Examples
+
+##### Example 1
+```
+Get-OZOHostname -FQDN "example.contoso.com"
+example
+```
+
+##### Example 2
+```
+Get-OZOHostname
+DESKTOP-OZO80202
+```
+
+#### Parameters
+|Parameter|Description|
+|---------|-----------|
+|`FQDN`|The fully qualified domain name.|
+
+#### Outputs
+System.String
