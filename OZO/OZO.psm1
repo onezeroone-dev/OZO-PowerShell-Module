80600c5d84c502b276c6e79404499376056f68fe
alievertz <andy.lievertz@gmail.com>
2024-12-04 23:51:19 -0700

    OZO substring functions (#5)
    * deleted PREREQUISITES.md
    
    * Added Get-OZODelimiterSubString, Get-OZOEndSubString, Get-OZOIndexSubString, and Get-OZOStartSubString; v1.3.0 release candidate
    
    ---------
    
    Co-authored-by: Andy Lievertz <alievertz@onezeroone.dev>

--- OZO/OZO.psm1
+++ OZO/OZO.psm1
@@ -7,10 +7,112 @@ Function Get-OZO64BitPowerShell {
         .EXAMPLE
         Get-OZO64BitPowerShell
         True
+        .OUTPUTS
+        System.Boolean
         .LINK
         https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozo64bitpowershell
     #>
     return [Environment]::Is64BitProcess
+}
+
+Function Get-OZODelimiterSubString {
+    <#
+        .SYNOPSIS
+        See description.
+        .DESCRIPTION
+        Parses a String and returns a List of substrings found between Start and End delimiters. If the start delimiter is not found, the List contains a single string leading up to the end delimiter. If the end delimiter is not found, the List contains a single string trailing the start delimiter. If neither delimiter is found, the List contains a single string (the original string).
+        .PARAMETER String
+        The string to process.
+        .PARAMETER Start
+        The start delimiter.
+        .PARAMETER End
+        The end delimiter.
+        .EXAMPLE
+        $subStringsList = Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start "{" -End "}"
+        $subStringsList[0]
+        226da830-da5c-42af-83fd-37467b753ec6
+        .EXAMPLE
+        (Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start "{" -End "}")[0]
+        226da830-da5c-42af-83fd-37467b753ec6
+        .EXAMPLE
+        Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start "{" -End "}" | Select-Object -First 1
+        226da830-da5c-42af-83fd-37467b753ec6
+        .EXAMPLE
+        $subStringsList = Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start ":" -End ":"
+        $subStringsList[0]
+        18
+        $subStringsList[1]
+        09-05
+        .OUTPUTS
+        System.Collections.Generic.List[String]
+        .NOTES
+        Thanks to this Stack Overflow post (https://stackoverflow.com/questions/67626879/system-collections-generic-liststring-as-return-value that helped me understand how to write a function that consistently returns a List.
+        .LINK
+        https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozodelimitersubstring
+    #>
+    # Parameters
+    [CmdLetBinding()]
+    param (
+        [Parameter(Mandatory=$true,HelpMessage="The string to parse")][String]$String,
+        [Parameter(Mandatory=$true,HelpMessage="The starting delimiter")][Char]$Start,
+        [Parameter(Mandatory=$true,HelpMessage="The ending delimiter")][Char]$End
+    )
+    # Variables
+    [System.Collections.Generic.List[String]]$Results = @()
+    # Determine if the string contains the Start and End characters
+    If ($String -Like ("*" + $Start + "*") -And $String -Like ("*" + $End + "*")) {
+        # String contains Start and End characters; add each of the results to the List
+        ForEach ($Result in [Regex]::Matches($String,"(?<=\$Start).+?(?=\$End)").Value) { $Results.Add($Result) }
+    # ElseIf determine if String contains the Start character
+    } ElseIf ($String -Like ("*" + $Start + "*")) {
+        # String contains the Start character; add the resulting string to the List
+        $Results.Add([Regex]::Matches($String,"(?<=\$Start).*").Value)
+    # ElseIf determine if String contains the End character
+    } ElseIf ($String -Like ("*" + $End + "*")) {
+        # String contains the End character; add the resulting string to the List
+        $Results.Add([Regex]::Matches($String,".*(?=\$End)").Value)
+    # Else add the original string to the List
+    } Else {
+        $Results.Add($String)
+    }
+    # Return
+    $PSCmdlet.WriteObject($Results)
+}
+
+Function Get-OZOEndSubString {
+    <#
+        .SYNOPSIS
+        See description.
+        .DESCRIPTION
+        Returns the last "Length" characters of a string. If Length is longer than String, the original string is returned.
+        .PARAMETER String
+        The string to parse. Accepts pipeline input.
+        .PARAMETER Length
+        The number of characters to return.
+        .EXAMPLE
+        Get-OZOEndSubString -String "The quick brown fox jumps over the lazy dog." -Length 8
+        azy dog.
+        .EXAMPLE
+        "The quick brown fox jumps over the lazy dog." | Get-OZOEndSubString -Length 8
+        azy dog.
+        .OUTPUTS
+        System.String
+        .LINK
+        https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozoendsubstring
+    #>
+    # Parameters
+    param (
+        [Parameter(Mandatory=$true,HelpMessage="The string to parse",ValueFromPipeline=$true)][String]$String,
+        [Parameter(Mandatory=$true,HelpMessage="The number of characters to return")][Int32]$Length
+    )
+    # Determine if Length is shorter than or equal to String length
+    If ($First -le $String.Length) { 
+        # Length is shorter than or equal to String length; return the desired substring
+        return $String.Substring($String.Length - $Length)
+    } Else {
+        # Length is *not* shorter than or equal to String length
+        return $String
+    }
 }
 
 Function Get-OZOHostname {
@@ -27,6 +129,8 @@ Function Get-OZOHostname {
         .EXAMPLE
         Get-OZOHostname
         DESKTOP-OZO80202
+        .OUTPUTS
+        System.String
         .LINK
         https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozohostname
     #>
@@ -44,12 +148,68 @@ Function Get-OZOHostname {
     }
 }
 
+Function Get-OZOIndexSubString {
+    <#
+        .SYNOPSIS
+        See description.
+        .DESCRIPTION
+        Returns the inner part of a string based on a starting index and length. If the length between the start index and the end of the string is longer than Length, the string trailing the start index is returned. If Length is longer than String, the original string is returned.
+        .PARAMETER String
+        The string to process. Accepts pipeline input.
+        .PARAMETER Index
+        The starting character index.
+        .PARAMETER Length
+        The number of characters to return.
+        .EXAMPLE
+        Get-OZOIndexSubString -String "The quick brown fox jumps over the lazy dog." -Index 8 -Length 4
+        ck b
+        .EXAMPLE
+        "The quick brown fox jumps over the lazy dog." | Get-OZOIndexSubString -Index 8 -Length 4
+        ck b
+        .LINK
+        https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozoindexsubstring
+    #>
+    # Parameters
+    param (
+        [Parameter(Mandatory=$true,HelpMessage="The string to parse",ValueFromPipeline=$true)][String]$String,
+        [Parameter(Mandatory=$true,HelpMessage="The starting character number")][Int32]$Index,
+        [Parameter(Mandatory=$true,HelpMessage="The number of characters to return")][Int32]$Length
+
+    )
+    # Determine if Start = or Start = 1
+    If ($Index -eq 0 -Or $Index -eq 1) {
+        # Start is 0 or 1; call Get-OZOStartSubString
+        return (Get-OZOStartSubString -String $String -Length $Length)
+    } ElseIf ($Index -eq $String.Length) {
+        # Start character is equal to the length of String; return the last character
+        return $String.Substring($String.Length - 1)
+    } ElseIf ($Index -lt $String.Length) {
+        # Shift Start to be inclusive
+        $Index = $Index - 1
+        # Determine that the start character is shorter than String length
+        If ($Index -lt $String.Length) {
+            # Start character shorter than String length; determine that the difference length is less than or equal to Length
+            If ($Length -le ($String.Length - $Index)) {
+                # Difference length is less than or equal to Length; return the desired substring
+                return $String.Substring($Index,$Length)
+            } Else {
+                # Difference length is *not* less than or equal to Length; return the substring starting at StartCharacter
+                return $String.Substring($String.Length - ($String.Length - $Index))
+            }
+
+        } Else {
+            # Start character is *not* less than the length of the string; return the original String
+            return $String
+        }
+    }
+}
+
 Function Get-OZONumberIsOdd {
     <#
         .SYNOPSIS
         See description.
         .DESCRIPTION
-        Takes a number as input and returns $true if the number is odd or $false if the number is even.
+        Evaluates an integer and returns True if the number is odd or False if the number is even.
         .PARAMETER Number
         The number to evaluate. Accepts pipeline input.
         .EXAMPLE
@@ -58,15 +218,51 @@ Function Get-OZONumberIsOdd {
         .EXAMPLE
         Get-OZONumberIsOdd -Number 4
         False
+        .OUTPUTS
+        System.Boolean
         .LINK
         https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozonumberisodd
     #>
     # Parameters
     param (
-        [Parameter(Mandatory=$true,HelpMessage="The number to evaluate",ValueFromPipeline=$true)][Float]$Number
+        [Parameter(Mandatory=$true,HelpMessage="The number to evaluate",ValueFromPipeline=$true)][Int32]$Number
     )
     # Return
     return [Boolean]($Number%2)
 }
 
-Export-ModuleMember -Function Get-OZO64BitPowerShell,Get-OZOHostname,Get-OZONumberIsOdd
+Function Get-OZOStartSubString {
+    <#
+        .SYNOPSIS
+        See description.
+        .DESCRIPTION
+        Returns the first "Length" characters of a string. If Length is longer than String, the original string is returned.
+        .PARAMETER String
+        The string to parse. Accepts pipeline input.
+        .PARAMETER Length
+        The number of characters to return.
+        .EXAMPLE
+        Get-OZOStartSubString -String "The quick brown fox jumps over the lazy dog." -Length 8
+        The quic
+        .EXAMPLE
+        "The quick brown fox jumps over the lazy dog." | Get-OZOStartSubString -Length 8
+        The quic
+        .LINK
+        https://github.com/onezeroone-dev/OZO-PowerShell-Module/blob/main/README.md#get-ozostartsubstring
+    #>
+    # Parameters
+    param (
+        [Parameter(Mandatory=$true,HelpMessage="The string to parse",ValueFromPipeline=$true)][String]$String,
+        [Parameter(Mandatory=$true,HelpMessage="The number of characters to return")][Int32]$Length
+    )
+    # Determine if Length shorter than or or equal to String length
+    If ($Length -le $String.Length) {
+        # Length is shorter than or equal to String length; return the desired substring
+        return $String.Substring(0,$Length)
+    } Else {
+        # Length is *not* shorter tahn equal to String length; return String
+        return $String
+    }
+}
+
+Export-ModuleMember -Function Get-OZO64BitPowerShell,Get-OZODelimiterSubString,Get-OZOEndSubString,Get-OZOHostname,Get-OZOIndexSubString,Get-OZONumberIsOdd,Get-OZOStartSubString
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # OZO PowerShell Module Installation and Usage
 
 ## Installation
-This module is published to the PowerShell Gallery. Register the repository and then execute the following in an _Administrator_ PowerShell:
+This module is published to [PowerShell Gallery](https://learn.microsoft.com/en-us/powershell/scripting/gallery/overview?view=powershell-5.1). Ensure your system is configured for this repository then execute the following in an _Administrator_ PowerShell:
 
 `Install-Module OZO`
 
@@ -12,10 +12,17 @@ Import this module in your script or console to make the functions available for
 
 ## Functions
 
+- [Get-OZO64BitPowerShell](#get-ozo64bitpowershell)
+- [Get-OZODelimiterSubString](#get-ozodelimitersubstring)
+- [Get-OZOEndSubString](#get-ozoendsubstring)
+- [Get-OZOHostname](#get-ozohostname)
+- [Get-OZOIndexSubString](#get-ozoindexsubstring)
+- [Get-OZONumberIsOdd](#get-ozonumberisodd)
+- [Get-OZOStartSubString](#get-ozostartsubstring)
+
 ### Get-OZO64BitPowerShell
 #### Description
 Returns True if the PowerShell environment is 64-bit and False if not.
-
 #### Syntax
 ```
 Get-OZO64BitPowerShell
@@ -30,7 +37,85 @@ None.
 #### Inputs
 None.
 #### Outputs
-System.Boolean
+`System.Boolean`
+
+---
+
+### Get-OZODelimiterSubString
+#### Description
+Parses a String and returns a List of substrings found between Start and End delimiters. If the start delimiter is not found, the List contains a single string leading up to the end delimiter. If the end delimiter is not found, the List contains a single string trailing the start delimiter. If neither delimiter is found, the List contains a single string (the original string).
+#### Syntax
+```
+Get-OZODelimiterSubString
+    -String <String>
+    -Start  <Int32>
+    -End    <Int32>
+```
+#### Examples
+##### Example 1
+```powershell
+$subStringsList = Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start "{" -End "}"
+$subStringsList[0]
+226da830-da5c-42af-83fd-37467b753ec6
+```
+##### Example 2
+```powershell
+(Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start "{" -End "}")[0]
+226da830-da5c-42af-83fd-37467b753ec6
+```
+##### Example 3
+```powershell
+Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start "{" -End "}" | Select-Object -First 1
+226da830-da5c-42af-83fd-37467b753ec6
+```
+##### Example 4
+```powershell
+$subStringsList = Get-OZODelimiterSubString -String "2023-06-20T14:18:09-05:00{226da830-da5c-42af-83fd-37467b753ec6}" -Start ":" -End ":"
+$subStringsList[0]
+18
+$subStringsList[1]
+09-05
+```
+#### Parameters
+|Parameter|Description|
+|---------|-----------|
+|`String`|The string to process.|
+|`Start`|The start delimiter.|
+|`End`|The end delimiter.|
+#### Outputs
+`System.Collections.Generic.List[String]`
+#### Notes
+Thanks to [this Stack Overflow post](https://stackoverflow.com/questions/67626879/system-collections-generic-liststring-as-return-value) for helping me understand how to write a function that consistently returns a List.
+
+---
+
+### Get-OZOEndSubString
+#### Description
+Returns the last "Length" characters of a string. If Length is longer than String, the original string is returned.
+#### Syntax
+```
+Get-OZOEndSubString
+    -String <String>
+    -Length <Int32>
+```
+#### Examples
+##### Example 1
+```powershell
+Get-OZOEndSubString -String "The quick brown fox jumps over the lazy dog." -Length 8
+azy dog.
+```
+##### Example 2
+```powershell
+"The quick brown fox jumps over the lazy dog." | Get-OZOEndSubString -Length 8
+azy dog.
+```
+#### Parameters
+|Parameter|Description|
+|---------|-----------|
+|`String`|The string to parse. Accepts pipeline input.|
+|`Length`|The number of characters to return.|
+#### Outputs
+`System.String`        
 
 ---
 
@@ -57,20 +142,50 @@ DESKTOP-OZO80202
 |Parameter|Description|
 |---------|-----------|
 |`FQDN`|The fully qualified domain name. Accepts pipeline input.|
-#### Inputs
-System.String
 #### Outputs
-System.String
+`System.String`
+
+---
+
+### Get-OZOIndexSubString
+#### Description
+Returns the inner part of a string based on a starting index and length. If the length between the start index and the end of the string is longer than Length, the string trailing the start index is returned. If Length is longer than String, the original string is returned.
+#### Syntax
+```
+Get-OZOIndexSubString
+    -String <String>
+    -Index  <Int32>
+    -Length <Int32>
+```
+#### Examples
+##### Example 1
+```powershell
+Get-OZOIndexSubString -String "The quick brown fox jumps over the lazy dog." -Index 8 -Length 4
+ck b
+```
+##### Example 2
+```powershell
+"The quick brown fox jumps over the lazy dog." | Get-OZOIndexSubString -Index 8 -Length 4
+ck b
+```
+#### Parameters
+|Parameter|Description|
+|---------|-----------|
+|`String`|The string to process. Accepts pipeline input.|
+|`Index`|The starting character index.|
+|`Length`|The number of characters to return.|
+#### Outputs
+`System.String`
 
 ---
 
 ### Get-OZONumberIsOdd
 #### Description
-Takes a number as input and returns $true if the number is odd or $false if the number is even.
+Evaluates an integer and returns True if the number is odd or False if the number is even.
 #### Syntax
 ```
 Get-OZONumberIsOdd
-    [-Number <Float>]
+    [-Number <Int32>]
 ```
 #### Examples
 ##### Example 1
@@ -86,8 +201,36 @@ False
 #### Parameters
 |Parameter|Description|
 |---------|-----------|
-|`Number`||The number to evaluate. Accepts pipeline input.|
-#### Inputs
-System.Float
+|`Number`|The number to evaluate. Accepts pipeline input.|
 #### Outputs
 System.Boolean
+
+---
+
+### Get-OZOStartSubString
+#### Description
+Returns the first "Length" characters of a string. If Length is longer than String, the original string is returned.
+#### Syntax
+```
+Get-OZOStartSubString
+    -String <String>
+    -Length <Int32>
+```
+#### Examples
+##### Example 1
+```powershell
+Get-OZOStartSubString -String "The quick brown fox jumps over the lazy dog." -Length 8
+The quic
+```
+##### Example 2
+```powershell
+"The quick brown fox jumps over the lazy dog." | Get-OZOStartSubString -Length 8
+The quic
+```
+#### Parameters
+|Parameter|Description|
+|---------|-----------|
+|`String`|The string to parse. Accepts pipeline input.|
+|`Length`|The number of characters to return.|
+#### Outputs
+`System.String`
